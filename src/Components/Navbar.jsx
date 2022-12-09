4201e56ef9dfc404588108f92ca4552538cf2bf9
rash1234567 <imamrasheedatahmad1993@gmail.com>
2022-12-09 02:22:33 -0800

    first commit for slick test

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-full md:h-[140px] h-[67px] flex bg-[#292929] items-center justify-center lg:justify-start'>
+        <div className='md:w-[150px] lg:mx-8 w-[103px] h-[33.58px] border-[1px] text-white  flex items-center justify-center border-white  border-solid md:h-[60px] lg:text-2xl md:text-xl text-lg lg:w-[193px]'>
+            <h1>MyTestApp</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
